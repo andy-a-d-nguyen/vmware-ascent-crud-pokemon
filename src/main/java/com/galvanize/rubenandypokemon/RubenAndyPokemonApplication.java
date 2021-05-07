810e6270879745256405c8b678532b446ef66fd7
RubenV-Dev <r.will16@hotmail.com>
2021-05-07 12:09:26 -0400

    initializr

---
+++ src/main/java/com/galvanize/rubenandypokemon/RubenAndyPokemonApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.rubenandypokemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubenAndyPokemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RubenAndyPokemonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galvanize/rubenandypokemon/RubenAndyPokemonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.rubenandypokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubenAndyPokemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
